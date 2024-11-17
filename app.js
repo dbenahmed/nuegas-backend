d9536da7ecac4268f47a597f6886db0d6f2c7125
dbenahmed <j.benahmedd@gmail.com>
2024-11-17 15:26:12 +0100

    tasks model validation / setters / schema update

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
-require("dotenv").config()
-const express = require('express')
-const mongoose = require('mongoose')
-const Projects = require('./models/projectsModel')
-const Users = require('./models/usersModel')
-const Comments = require('./models/commentsModel')
-const app = express()
-const port = 9990
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Projects = require("./models/projectsModel");
 
-mongoose.set('debug', true);
-app.use(express.json())
+const Users = require("./models/usersModel");
+const Comments = require("./models/commentsModel");
+const Tasks = require('./models/tasksModel')
+
+const app = express();
+const port = 9990;
+
+mongoose.set("debug", true);
+app.use(express.json());
 
 // TESTING ROUTE
-app.use('/', (req, res) => {
-        
-    res.send()
-})
+app.use("/", (req, res) => {
+    Tasks.create({
+        name: 'test a e',
+        dueDate: Date.now(),
+        priority: 'high',   
+    })
+    res.send();
+});
 
 app.listen(port, async () => {
     try {
-        const uri = process.env.MONGODB_URI
-        if (!uri) { throw ('uri unvalid') }
-        await mongoose.connect(uri)
-        console.log('server connected')
+        const uri = process.env.MONGODB_URI;
+        if (!uri) {
+            throw "uri unvalid";
+        }
+        await mongoose.connect(uri);
+        console.log("server connected");
     } catch (e) {
-        console.log(e)
+        console.log(e);
     }
-})
+});
--- models/tasksModel.js
+++ models/tasksModel.js
@@ -1,29 +1,52 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model, Types } = require("mongoose");
 
 const taskSchema = new Schema({
-    name: { type: String },
-    dueDate: { type: Date }
-    , deadlineDate: { type: Date },
-    parentProjects: {
-        type: [String],
-        default: []
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 16,
+        match: /^[A-Za-z0-9 ]+$/
     },
-    priority: { type: String },
+    dueDate: { type: Date },
+    deadlineDate: { type: Date },
+    parentProjects: {
+        type: [Types.ObjectId],
+        default: [],
+        required: true,
+    },
+    priority: {
+        type: String,
+        enum: {
+            values: ["urgent", "high", "medium", "low", "no-priority"],
+            message: (v) => `${v} should be [high,low,medium,no-priority,urgent]`,
+        },
+        required: true,
+        default: "no-priority",
+        trim: true,
+    },
     status: {
         type: String,
-        required: true
+        required: true,
+        default: 'backlog',
+        trim: true,
+        enum: {
+            values: ["todo", "done", "doing", "cancelled", "backlog", "in-review"],
+            message: (v) => {
+                return `${v} should be either [done,doing,in-review,cancelled,backlog,todo]`;
+            },
+        },
     },
-    assignee: { type: [String] },
+    assignee: { type: [Types.ObjectId] },
     createdAt: {
         type: Date,
         default: Date.now(),
-        immutable: true
+        immutable: true,
     },
     updatedAt: {
         type: Date,
-        default: Date.now()
-    }
-})
+        default: Date.now(),
+    },
+});
 
-
-module.exports = model('Tasks', taskSchema)
+module.exports = model("Tasks", taskSchema);
