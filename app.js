3be44fc13ab040d84f74828a410d04a22f2eaa43
dbenahmed <j.benahmedd@gmail.com>
2024-11-17 14:19:56 +0100

    made comments schem validators, setters

--- app.js
+++ app.js
@@ -3,24 +3,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Projects = require('./models/projectsModel')
 const Users = require('./models/usersModel')
+const Comments = require('./models/commentsModel')
 const app = express()
-const port = 2346
+const port = 9990
 
 mongoose.set('debug', true);
 app.use(express.json())
 
 // TESTING ROUTE
 app.use('/', (req, res) => {
-    const newP = new Projects ({
-        name: 'testName   ',
-        projectTasks: ['111', '222'],
-        members: ['newmember'],
-        description: ' test    ',
-        createdBy: 'testname'
-    })
-    console.log('getting')
-    newP.changeName('newNaemhere')
-    res.send(newP)
+        
+    res.send()
 })
 
 app.listen(port, async () => {
--- models/commentsModel.js
+++ models/commentsModel.js
@@ -1,14 +1,39 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model, Types } = require('mongoose')
+
 const commentSchema = new Schema({
     content: {
         type: String,
+        maxLength: [280, 'comment should have a length that is less or equal to 280'],
         required: true,
+        match: [/^[a-zA-Z0-9\s-]+$/, 'name must not contain special characters beside -'],
+        trim: true,
     },
-    parentProject: { type: String, required: true },
-    parentTask: { type: String, required: true },
-    createdBy: { type: String, required: true },
+    parent: {
+        parentType: {
+            type: String,
+            enum: ['project', 'task'],
+            required: true,
+            immutable:true
+        },
+        parentId: {
+            type: Types.ObjectId,
+            trim: true,
+            required: true,
+            immutable:true
+        }
+    },
+    createdBy: { type: Types.ObjectId, required: true, immutable: true, trim: true, },
     createdAt: { type: Date, default: Date.now(), immutable: true },
     updatedAt: { type: Date, default: Date.now() }
+}, {
+    methods: {
+        updateContent(newContent) {
+            this.content = newContent
+        },
+        updateUpdatedAt(date) {
+            this.updatedAt = date
+        }
+    }
 })
 
 module.exports = model('Comments', commentSchema)
