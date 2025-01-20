0d5c1a1b7d3f63b53a333bb230c67dbd01acf7f3
dbenahmed <j.benahmedd@gmail.com>
2025-01-20 14:42:52 +0100

    Fixing the error handler function to show the mongoose errors to the API RESPONSE

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,12 +2,15 @@ const ApiError = require('../utils/ApiError.js')
 
 const errorHandler = (err, req, res, next) => {
     let response
-    if (!Number.isInteger(err.statusCode)) {
-        res.status(500).json({
-            success: false, message: `${err.statusCode} is not a number`
-        })
-    }
+    // check if the given status code is a actual number if not send a server error
+
+    // if the error is a custom error made by the code and not an SDK ( mongoose or another library )
     if (err instanceof ApiError) {
+        if (!Number.isInteger(err.statusCode)) {
+            res.status(500).json({
+                success: false, message: `${err.statusCode} is not a number`
+            })
+        }
         response = err.toJson()
         res.status(err.statusCode || 500);
     } else {
